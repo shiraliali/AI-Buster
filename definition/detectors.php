a9afa195e1599eb11e6fd76e47b46e6559fd616f
ali shirali <inboxalishirali@gmail.com>
2024-02-05 15:53:58 +0330

    add: detector core updated with improved pattern

--- definition/detectors.php
+++ definition/detectors.php
@@ -3,7 +3,7 @@
 define('CODEDETECTION', '/[A-Za-z0-9_]+\(|\$[A-Za-z0-9_]+|class |def |function |public |static |new |echo |print |#include |#define |using /m');
 define('SPACECRAFT', '/^(\s*\n){3,}/m');
 define('SPACEHUNT', '/\s+/u');
-define('REGEXFOOLNESS', '(loop|print|show|res|iteration|error|handler|info|arr|temp|obj|val|value|index|size|qty|param|cb|callback|config|found|search|arg|arguman|code|product|counter|yes|no|conn|foo|bar|baz|you|your|data|key|test|my|first|second|total|number|last|comment|bad|good|positive|negative|possible|user|computer|upload|folder|loging|logging|product|output|result|sum|end|regex|string|text|clean|here|yourdomain|mydomain|example|domain|close|calculate|tag|percent|words|num|formatter|format|[a-z])');
+define('REGEXFOOLNESS', '(loop|print|show|res|iteration|error|handler|info|arr|temp|obj|val|value|index|size|qty|param|cb|callback|config|found|search|arg|arguman|code|product|counter|yes|no|conn|foo|bar|baz|you|your|data|key|test|my|first|second|total|number|last|comment|bad|good|positive|negative|possible|user|computer|upload|folder|loging|logging|product|output|result|sum|end|regex|string|text|clean|here|yourdomain|mydomain|example|usage|domain|close|calculate|tag|percent|words|num|formatter|format|[a-z])');
 define('FOOLMESS', '/<(?:\?(?=php)|[%?!]|script)>/m');
 define('FOOLNESS', '/[$|_|\b]' . REGEXFOOLNESS . '(\b|;)/i');
 define('FOOLNESS_OTHERS', '/\b' . REGEXFOOLNESS . '\b/i');
